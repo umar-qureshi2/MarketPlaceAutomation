3a54871d909d125bbaa5769b60a8f264d1bfb3c5
Umar Qureshi <umar.qureshi@trasix.com>
2019-12-12 16:31:55 +0400

    small program to add API route name in DB script. Also adds permissions for admin user to access API. used frquently and works well for merging with other developers

---
+++ AddApiToMarketplace/AddApiToMarketplace/EntryForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace AddApiToMarketplace
+{
+    partial class EntryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddApiToDatabaseButton = new System.Windows.Forms.Button();
+            this.ApiName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Description = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AddApiToDatabaseButton
+            // 
+            this.AddApiToDatabaseButton.Location = new System.Drawing.Point(647, 52);
+            this.AddApiToDatabaseButton.Name = "AddApiToDatabaseButton";
+            this.AddApiToDatabaseButton.Size = new System.Drawing.Size(116, 23);
+            this.AddApiToDatabaseButton.TabIndex = 0;
+            this.AddApiToDatabaseButton.Text = "Add to database";
+            this.AddApiToDatabaseButton.UseVisualStyleBackColor = true;
+            this.AddApiToDatabaseButton.Click += new System.EventHandler(this.AddToDatabaseButton_Click);
+            // 
+            // ApiName
+            // 
+            this.ApiName.Location = new System.Drawing.Point(60, 52);
+            this.ApiName.Name = "ApiName";
+            this.ApiName.Size = new System.Drawing.Size(168, 20);
+            this.ApiName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Api Name";
+            // 
+            // Description
+            // 
+            this.Description.Location = new System.Drawing.Point(60, 112);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(168, 20);
+            this.Description.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Api Description";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(332, 92);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(0, 13);
+            this.status.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Description);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ApiName);
+            this.Controls.Add(this.AddApiToDatabaseButton);
+            this.Name = "Form1";
+            this.Text = "A";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddApiToDatabaseButton;
+        private System.Windows.Forms.TextBox ApiName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Description;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ AddApiToMarketplace/AddApiToMarketplace/EntryForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddApiToMarketplace
+{
+    public partial class EntryForm : Form
+    {
+        public EntryForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Adds API entry to database script on basis of recent most id from script and current formatting
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddToDatabaseButton_Click(object sender, EventArgs e)
+        {
+            status.Text = "Starting";
+            try
+            {
+                string webApisDflt = string.Empty;
+                int previousMax = 0;
+                string newApiText = "";
+                using (StreamReader sr = new StreamReader(@"D:\Trasix\MarketPlace\MP.Database\dbo\InitialData\Script.webapis_dflt.sql"))
+                {
+                    webApisDflt = sr.ReadToEnd();
+                    previousMax = webApisDflt.Split(new string[] { "id =" }, StringSplitOptions.RemoveEmptyEntries)
+                        .Where(x => Regex.IsMatch(x, @".*[0-9]+\)"))
+                        .Select(x => x.Trim())
+                        .Select(x =>
+                        {
+                            var index = x.IndexOf(')');
+                            var split = x.Substring(0, index);
+                            return Convert.ToInt32(split);
+                        })
+                        .Max();
+                    if (previousMax > 0)
+                    {
+                        var newIndex = ++previousMax;
+                        newApiText = $"\r\nIF EXISTS (SELECT 1 FROM dbo.[webapis_dflt] WHERE id = {newIndex}) BEGIN  UPDATE [dbo].[webapis_dflt] SET [discription]=N'{Description.Text}', [RouteName]=N'{ApiName.Text}' WHERE ID = {newIndex} END ELSE BEGIN  INSERT [dbo].[webapis_dflt] (id, [discription], [RouteName])VALUES ({newIndex}, N'{Description.Text}', N'{ApiName.Text}') END";
+                    }
+                }
+                if (previousMax > 0)
+                {
+                    var newIndex = previousMax;
+                    using (StreamWriter sw = new StreamWriter(@"D:\Trasix\MarketPlace\MP.Database\dbo\InitialData\Script.webapis_dflt.sql", true))
+                    {
+                        sw.Write(newApiText);
+                    }
+
+                    string userRolesWebApis = "";
+                    using (StreamReader sr = new StreamReader(@"D:\Trasix\MarketPlace\MP.Database\dbo\InitialData\Script.userrole_webapis_relation.sql"))
+                    {
+                        userRolesWebApis = sr.ReadToEnd();
+                        userRolesWebApis = userRolesWebApis.Replace("GO", $"IF NOT EXISTS (SELECT 1 FROM dbo.[userrole_webapis_relation] WHERE [user_role_id] = @roleId  AND [webapiid] ={newIndex})BEGIN INSERT INTO [dbo].[userrole_webapis_relation]([user_role_id],[webapiid],[data_filter],[created_by],[updated_by],[created_date],[update_date],[regularexpression])VALUES(@roleId,{newIndex},NULL,1,1,GETDATE(),GETDATE(),NULL) END\r\nGO");
+                    }
+
+                    using (StreamWriter sw = new StreamWriter(@"D:\Trasix\MarketPlace\MP.Database\dbo\InitialData\Script.userrole_webapis_relation.sql"))
+                    {
+                        sw.Write(userRolesWebApis);
+                    }
+
+                }
+                status.Text = "done";
+            }
+            catch (Exception exception)
+            {
+                status.Text = exception.Message;
+                MessageBox.Show(exception.Message + Environment.NewLine + exception.StackTrace);
+            }
+        }
+    }
+}
