a252b77bff4480a25449881e925437399c207c11
Umar Qureshi <umar.qureshi@trasix.com>
2019-12-17 16:36:03 +0400

    commiting the initial version. create input/output doc from swift code for classes to be used with MP template

---
+++ QuickTypeJsonToDocumentation/QuickTypeJsonToDocumentation/EntryForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace QuickTypeJsonToDocumentation
+{
+    partial class JsonToDocumentation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.OutputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.GenerateDocBtn = new System.Windows.Forms.Button();
+            this.GenerateOutputDocBtn = new System.Windows.Forms.Button();
+            this.EntityName = new System.Windows.Forms.TextBox();
+            this.UIGroup = new System.Windows.Forms.GroupBox();
+            this.UIGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InputRichTextBox
+            // 
+            this.InputRichTextBox.Location = new System.Drawing.Point(37, 13);
+            this.InputRichTextBox.Name = "InputRichTextBox";
+            this.InputRichTextBox.Size = new System.Drawing.Size(462, 207);
+            this.InputRichTextBox.TabIndex = 0;
+            this.InputRichTextBox.Text = "";
+            // 
+            // OutputRichTextBox
+            // 
+            this.OutputRichTextBox.Location = new System.Drawing.Point(37, 222);
+            this.OutputRichTextBox.Name = "OutputRichTextBox";
+            this.OutputRichTextBox.Size = new System.Drawing.Size(462, 207);
+            this.OutputRichTextBox.TabIndex = 1;
+            this.OutputRichTextBox.Text = "";
+            // 
+            // GenerateDocBtn
+            // 
+            this.GenerateDocBtn.Location = new System.Drawing.Point(515, 19);
+            this.GenerateDocBtn.Name = "GenerateDocBtn";
+            this.GenerateDocBtn.Size = new System.Drawing.Size(142, 23);
+            this.GenerateDocBtn.TabIndex = 2;
+            this.GenerateDocBtn.Text = "Generate Input Doc";
+            this.GenerateDocBtn.UseVisualStyleBackColor = true;
+            this.GenerateDocBtn.Click += new System.EventHandler(this.GenerateDocBtn_Click);
+            // 
+            // GenerateOutputDocBtn
+            // 
+            this.GenerateOutputDocBtn.Location = new System.Drawing.Point(515, 48);
+            this.GenerateOutputDocBtn.Name = "GenerateOutputDocBtn";
+            this.GenerateOutputDocBtn.Size = new System.Drawing.Size(142, 23);
+            this.GenerateOutputDocBtn.TabIndex = 3;
+            this.GenerateOutputDocBtn.Text = "Generate Output Doc";
+            this.GenerateOutputDocBtn.UseVisualStyleBackColor = true;
+            this.GenerateOutputDocBtn.Click += new System.EventHandler(this.GenerateOutputDocBtn_Click);
+            // 
+            // EntityName
+            // 
+            this.EntityName.Location = new System.Drawing.Point(515, 90);
+            this.EntityName.Name = "EntityName";
+            this.EntityName.Size = new System.Drawing.Size(142, 20);
+            this.EntityName.TabIndex = 4;
+            // 
+            // UIGroup
+            // 
+            this.UIGroup.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UIGroup.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.UIGroup.Controls.Add(this.OutputRichTextBox);
+            this.UIGroup.Controls.Add(this.EntityName);
+            this.UIGroup.Controls.Add(this.InputRichTextBox);
+            this.UIGroup.Controls.Add(this.GenerateOutputDocBtn);
+            this.UIGroup.Controls.Add(this.GenerateDocBtn);
+            this.UIGroup.Location = new System.Drawing.Point(8, 5);
+            this.UIGroup.Name = "UIGroup";
+            this.UIGroup.Size = new System.Drawing.Size(971, 441);
+            this.UIGroup.TabIndex = 5;
+            this.UIGroup.TabStop = false;
+            this.UIGroup.Text = "groupBox1";
+            // 
+            // JsonToDocumentation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 450);
+            this.Controls.Add(this.UIGroup);
+            this.Name = "JsonToDocumentation";
+            this.Text = "JsonToDocumentation";
+            this.UIGroup.ResumeLayout(false);
+            this.UIGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox InputRichTextBox;
+        private System.Windows.Forms.RichTextBox OutputRichTextBox;
+        private System.Windows.Forms.Button GenerateDocBtn;
+        private System.Windows.Forms.Button GenerateOutputDocBtn;
+        private System.Windows.Forms.TextBox EntityName;
+        private System.Windows.Forms.GroupBox UIGroup;
+    }
+}
+
---
+++ QuickTypeJsonToDocumentation/QuickTypeJsonToDocumentation/EntryForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace QuickTypeJsonToDocumentation
+{
+    public partial class JsonToDocumentation : Form
+    {
+        public JsonToDocumentation()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateDocBtn_Click(object sender, EventArgs e)
+        {
+            //var inputText = InputRichTextBox.Text;
+            GenerateDoc(true);
+        }
+
+        private void GenerateDoc(bool isInputMode = false)
+        {
+            var inputText = InputRichTextBox.Text;
+            var lines = inputText.Replace("\tlet ", "")
+                                  .Replace("    let ", "")
+                                  .Replace(" ", "")
+                                  .Split('\r','\n')
+                                  .Where(x=>!string.IsNullOrWhiteSpace(x));
+            StringBuilder outputDoc = new StringBuilder();
+            foreach (var line in lines)
+            {
+                var properties = line.Split(':');
+                var dataType = properties.Last();
+                if (dataType.Contains("["))
+                {
+                    dataType = $"{dataType.Trim('[', ']')}Array";
+                }
+                foreach (var property in properties.First().Split(','))
+                {
+                    var propertySentence = ToSentenceCase(property);
+                    var documentLine = $"{propertySentence}\t{dataType}\t{GetExpectedValue(dataType)}\t{GetTypeDescription(dataType, propertySentence, isInputDescription: isInputMode)}";
+                    outputDoc.AppendLine(documentLine);
+                }
+            }
+
+            OutputRichTextBox.Text = outputDoc.ToString();
+        }
+
+        private string GetTypeDescription(string dataType, string propertyName, bool isInputDescription)
+        {
+            var entityText = EntityName.Text;
+            var result = dataType;
+            var idString = string.IsNullOrWhiteSpace(propertyName.Replace("Id", "")) ? entityText : propertyName.Replace("Id", "");
+            if (dataType.StartsWith("Int"))
+                result = isInputDescription ? $"Integer value to be assigned as {propertyName} for {entityText}"
+                    : $"{dataType} value for {propertyName} "
+                    + (propertyName.Contains("Id") ? $"Id for {idString}" : "")
+                    + (propertyName.Contains("Is") || propertyName.ToLower().Contains("status") ? $"Can be 0 or 1" : "")
+                    + (propertyName.Contains("By") ? $"References user who {propertyName.Replace("By", "")} the {entityText}" : "");
+            if (dataType.StartsWith("String"))
+                result = isInputDescription ? $"String text value to be assigned as {propertyName} for {entityText}"
+                    : $"{propertyName} value for {entityText} "
+                    + (propertyName.Contains("Date") ? $"{propertyName.Replace("Date", "")} Date in UTC \"yyyy-MM-ddTHH:mm:ss.fffZ\" format" : "");
+
+            return result;
+        }
+
+        /// <summary>
+        /// does not consider the culture and locale
+        /// </summary>
+        /// <param name="dataType"></param>
+        /// <returns></returns>
+        private string GetExpectedValue(string dataType)
+        {
+            var result = dataType;
+            if (dataType.StartsWith("Int"))
+                result = "Positive Integer or Zero";
+            if (dataType.StartsWith("String"))
+                result = "Text or unicode characters";
+            if (dataType.StartsWith("Bool"))
+                result = "Boolean or truthy value";
+
+            if (dataType.EndsWith("Array"))
+                result = "Array of " + result.Replace("Array","");
+            return result;
+        }
+
+        private string ToSentenceCase(string source)
+        {
+
+            return source.First().ToString().ToUpper() + source.Substring(1); //capitalize first letter
+        }
+
+
+        string sampleText = @"    let channel, objectID, title, comment: String
+    let settings: String";
+
+        private void GenerateOutputDocBtn_Click(object sender, EventArgs e)
+        {
+            GenerateDoc();
+        }
+    }
+}
